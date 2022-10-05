8d888e97c9c09b836a85cc16655daae335dddf22
Samuel dos Santos Alencar <51748157+Samuel-sys@users.noreply.github.com>
2022-10-04 23:57:41 -0300

    Inclusão da funções de Back-end no site. Criado a conexão com a API do pokemon e realizada a trativa no arquivo JSON e mostrando o gif do pokemon além de apresentar o nome e ID do pokemon para o usuario

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const PokeName = document.querySelector('.pokemon_name');
+const PokeNumber =document.querySelector('.pokemon_number');
+const PokeImage =document.querySelector('.pokemon_image');
+
+const form =document.querySelector('.form');
+const PokeSearch =document.querySelector('.input_search');
+
+const fetchPokemon = async (pokemon) =>{
+
+    /*Estamos realizado o processo de consulta na PokeAPI de um pokemon e estamos aguardando
+    Recebimento dos dados do pokemon */
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    const data = await APIResponse.json(); //extraimos os dados JSONG
+}
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    //Estamos pegando o arquivo JSON e extraindo os dados
+    PokeName.innerHTML = data.name;
+    PokeNumber.innerHTML = data.id;
+    PokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front-default'];
+
+}
+
+//Estamos vinculando o evento "submit" a função de renderPokemon para estarmos buscando o pokemon no banco e apresentando os dados
+form.addEventListener('submit', (event)=>{
+
+    event.preventDefault();
+
+    renderPokemon(PokeSearch.value);
+
+    PokeSearch.value = '';
+
+});
