e455ad3ea66d88564c69cd84969a3f89e9c84899
Samuel-sys <Alencar5647@gmail.com>
2022-10-06 19:03:49 -0300

    Correção na falha do gif do pokemon não aparecer e realizada a configuração para que toda a vez que o sistema iniciar ele ter de mostrar o Bulbasaur e permitir que o usuário consulte o pokemon pelo nome.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const PokeName = document.querySelector('.pokemon_name');
-const PokeNumber =document.querySelector('.pokemon_number');
-const PokeImage =document.querySelector('.pokemon_image');
+const PokeNumber = document.querySelector('.pokemon_number');
+const PokeImage = document.querySelector('.pokemon_image');
 
-const form =document.querySelector('.form');
-const PokeSearch =document.querySelector('.input_search');
+const form = document.querySelector('.form');
+const PokeSearch = document.querySelector('.input_search');
 
-const fetchPokemon = async (pokemon) =>{
+const fetchPokemon = async (pokemon) => {
 
     /*Estamos realizado o processo de consulta na PokeAPI de um pokemon e estamos aguardando
     Recebimento dos dados do pokemon */
@@ -18,22 +18,30 @@ const fetchPokemon = async (pokemon) =>{
 
 const renderPokemon = async (pokemon) => {
 
+    pokemon = pokemon == '' || pokemon == null ? '1' : pokemon;
+
+    pokemon = pokemon.toString().toLowerCase();
+
     const data = await fetchPokemon(pokemon);
 
     //Estamos pegando o arquivo JSON e extraindo os dados
     PokeName.innerHTML = data.name;
     PokeNumber.innerHTML = data.id;
-    PokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front-default'];
+    //Caminho até o gif do pokemon para aparecer no site
+    PokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
 }
 
 //Estamos vinculando o evento "submit" a função de renderPokemon para estarmos buscando o pokemon no banco e apresentando os dados
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) => {
 
     event.preventDefault();
 
-    renderPokemon(PokeSearch.value == '' || PokeSearch.value == null ? 1 : PokeSearch.value);
-
+    renderPokemon(PokeSearch.value);
     PokeSearch.value = '';
 
 });
+
+window.onload = () => {
+    renderPokemon();
+}
