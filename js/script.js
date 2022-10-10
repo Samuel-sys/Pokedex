e8733fc4cdfb8e99b0090d9db01956f3e3a0b368
Samuel-sys <Alencar5647@gmail.com>
2022-10-10 13:23:25 -0300

    Adição do evento de navegação da pokedex pelas setas

--- js/script.js
+++ js/script.js
@@ -113,9 +113,14 @@ form.addEventListener('submit', (event) => {
 
 //butões de navegação da pokedex (realizamos a consulta do pokemon anterio ou seguinte da lista)
 ButtonNext.addEventListener('click', () => renderPokemon(idPokemon + 1));
-//document.addEventListener('keydown', (vl) => renderPokemon(idPokemon + 1));
+document.addEventListener('keydown', //toma da de decisão usando condição ternaria onde que dependendo da tecla que o usuario clicar o sistema irá ou não executar o bloco
+    (x) => x.key == 'ArrowRight' ?              //true
+        renderPokemon(idPokemon + 1) : null);   //false
 
 ButtonPrev.addEventListener('click', () => renderPokemon(idPokemon > 1 ? idPokemon - 1 : idPokemon));
+document.addEventListener('keydown',
+    (x) => x.key == 'ArrowLeft' ?
+        renderPokemon(idPokemon > 1 ? idPokemon - 1 : idPokemon) : null);
 
 window.onload = () => {
     renderPokemon();
