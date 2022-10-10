6424d6cfc3a8c9781712a5ced0ba507a6c036199
Samuel-sys <Alencar5647@gmail.com>
2022-10-10 12:56:55 -0300

    Adicionado o script para apresentação o pokemon de frente e de costas e adicionada a opção de mostrar uma foto de default caso o pokemon não tenha uma foto cadastrada na API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const PokeName = document.querySelector('.pokemon_name');
 const PokeNumber = document.querySelector('.pokemon_number');
 const PokeImage = document.querySelector('.pokemon_image');
+const TwoPokeImage = document.querySelectorAll('.pokemon_image2');
 
 const form = document.querySelector('.form');
 const PokeSearch = document.querySelector('.input_search');
@@ -12,24 +13,36 @@ let idPokemon = 1;
 //Butão de ativação para que o usuario possa ver o pokemon no modo shiny
 const btnShiny = document.querySelector('.shiny');
 let shiny = false;
-btnShiny.addEventListener('click',() => shiny = activatedButton(shiny,btnShiny));
+btnShiny.addEventListener('click', () => shiny = activatedButton(shiny, btnShiny));
 
 const btnPosicion = document.querySelector('.posicion');
 let posicion = false;
-btnPosicion.addEventListener('click',() => posicion = activatedButton(posicion,btnPosicion));
+btnPosicion.addEventListener('click', () => posicion = activatedButton(posicion, btnPosicion));
 
-const btnFull = document.querySelector('.full');
-let full = false;
-btnFull.addEventListener('click',() => full = activatedButton(full,btnFull));
+const btnFrontAndBack = document.querySelector('.FrontAndBack');
+let FrontAndBack = false;
+btnFrontAndBack.addEventListener('click', () => {
+    FrontAndBack = activatedButton(FrontAndBack, btnFrontAndBack);
+    if (FrontAndBack) {
+        PokeImage.classList.add('hide');
+        TwoPokeImage[0].classList.remove('hide');
+        TwoPokeImage[1].classList.remove('hide');
+    }
+    else {
+        PokeImage.classList.remove('hide');
+        TwoPokeImage[0].classList.add('hide');
+        TwoPokeImage[1].classList.add('hide');
+    }
+});
 
 //Essa função irá adicionar ou remover a classe "activated" do elemento que foi informado. 
 //para definir isso sempre entre com o valor bool que terá a inversão de seu valor
-const activatedButton = (status,elemnt)=>{
+const activatedButton = (status, elemnt) => {
     status = !status;
 
-    if(status){
+    if (status) {
         elemnt.classList.add('activated');
-    }else{
+    } else {
         elemnt.classList.remove('activated');
     }
 
@@ -38,6 +51,42 @@ const activatedButton = (status,elemnt)=>{
     return status;
 };
 
+const renderPokemon = async (pokemon) => {
+
+    //Caso o colaborador não escreva nada ele consulta o pokemon de ID 1
+    pokemon = pokemon == '' || pokemon == null ? '1' : pokemon;
+    //Permite o usuário a fazer consultas pelo nome do pokemon independente se ele escrever com tudo em maiúsculo ou minusculo
+    pokemon = pokemon.toString().toLowerCase();
+
+    //consulta na API o pokemon informado e retorna o JSON com todas as informações do pokemon
+    const data = await fetchPokemon(pokemon);
+
+    var img = data['sprites']['versions']['generation-v']['black-white']['animated'];
+
+    /*Estamos definindo qual a forma da apresentação do pokemon na pokedex */
+    var posicionJSON = posicion ? 'back_' : 'front_';
+    var shinyJSON = shiny ? 'shiny' : 'default';
+
+    if (FrontAndBack) {
+        var img1 = img['back_' + shinyJSON];
+        var img2 = img['front_' + shinyJSON];
+
+        TwoPokeImage[0].src = img1 ? img1 : "./images/img_default.png";
+        TwoPokeImage[1].src = img2 ? img2 : "./images/img_default.png";
+    }
+    else {
+        var img = img[posicionJSON + shinyJSON];
+        //Caminho até o gif do pokemon para aparecer no site
+        PokeImage.src = img ? img : "./images/img_default.png";
+    }
+
+    //Estamos pegando o arquivo JSON e extraindo os dados
+    PokeName.innerHTML = data.name;
+    PokeNumber.innerHTML = data.id;
+    idPokemon = data.id;
+
+
+}
 
 const fetchPokemon = async (pokemon) => {
 
@@ -52,28 +101,6 @@ const fetchPokemon = async (pokemon) => {
     }
 }
 
-const renderPokemon = async (pokemon) => {
-
-    //Caso o colaborador não escreva nada ele consulta o pokemon de ID 1
-    pokemon = pokemon == '' || pokemon == null ? '1' : pokemon;
-    //Permite o usuário a fazer consultas pelo nome do pokemon independente se ele escrever com tudo em maiúsculo ou minusculo
-    pokemon = pokemon.toString().toLowerCase();
-
-    const data = await fetchPokemon(pokemon);
-
-    /*Estamos definindo qual a forma da apresentação do pokemon na pokedex */
-    var pkm = posicion ? 'back_' : 'front_';
-    pkm += shiny ? 'shiny': 'default';
-
-    //Estamos pegando o arquivo JSON e extraindo os dados
-    PokeName.innerHTML = data.name;
-    PokeNumber.innerHTML = data.id;
-    idPokemon = data.id;
-    //Caminho até o gif do pokemon para aparecer no site
-    PokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated'][pkm];
-
-}
-
 //Estamos vinculando o evento "submit" a função de renderPokemon para estarmos buscando o pokemon no banco e apresentando os dados
 form.addEventListener('submit', (event) => {
 
@@ -85,12 +112,10 @@ form.addEventListener('submit', (event) => {
 });
 
 //butões de navegação da pokedex (realizamos a consulta do pokemon anterio ou seguinte da lista)
-ButtonNext.addEventListener('click', () => {
-    renderPokemon(idPokemon +1);
-});
-ButtonPrev.addEventListener('click', () => {
-    renderPokemon(idPokemon > 1 ? idPokemon - 1: idPokemon);
-});
+ButtonNext.addEventListener('click', () => renderPokemon(idPokemon + 1));
+//document.addEventListener('keydown', (vl) => renderPokemon(idPokemon + 1));
+
+ButtonPrev.addEventListener('click', () => renderPokemon(idPokemon > 1 ? idPokemon - 1 : idPokemon));
 
 window.onload = () => {
     renderPokemon();
